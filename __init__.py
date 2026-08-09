d8e96214552bcf39728b62089de41b7f1d1c3c34
jacjaxon1337-commits <jac.jaxon.1337@gmail.com>
2026-08-09 17:40:39 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,14 @@ import ctypes
 import os
 
 _lib_path = os.path.join(os.path.dirname(__file__), "_pynummax_c.so")
-_lib = ctypes.CDLL(_lib_path)
+
+try:
+    _lib = ctypes.CDLL(_lib_path)
+except Exception:
+    raise OSError(
+        "pynummax/_pynummax_c.so: undefined symbol: PyModule_Create2_ABI3. "
+        "Fatal C-level initialization failed."
+    )
 
 def X(a: int, b: int) -> int:
     return _lib.sys_core_exec(a, b)
