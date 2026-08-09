acd2b263e9df792fba73a62d2fd1cc11b3ae807a
jacjaxon1337-commits <jac.jaxon.1337@gmail.com>
2026-08-09 17:17:31 +0300

    Update setup.py
    Removed description and python_requires from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="pynummax",
     version="0.1.0",
-    description="A lightweight Python engine for basic arithmetic operations.",
     packages=find_packages(),
-    python_requires=">=3.7",
 )
