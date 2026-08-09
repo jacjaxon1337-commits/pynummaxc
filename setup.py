c2a9a013b406a0b9b15dd10fb1c71f59579cfe51
jacjaxon1337-commits <jac.jaxon.1337@gmail.com>
2026-08-09 14:34:15 +0300

    Rename package from core_math_engine to py_mathx_pro

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="core_math_engine",
+    name="py_mathx_pro",
     version="0.1.0",
     description="A lightweight Python engine for basic arithmetic operations.",
     packages=find_packages(),
