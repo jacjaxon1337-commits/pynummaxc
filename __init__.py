b14cb3bff2b2fd56773bc345e18962591cadf2db
jacjaxon1337-commits <jac.jaxon.1337@gmail.com>
2026-08-09 17:21:19 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
-from .core import X
+import ctypes
+import os
 
-__all__ = ["X"]
+_lib_path = os.path.join(os.path.dirname(__file__), "_pynummax_c.so")
+_lib = ctypes.CDLL(_lib_path)
+
+def X(a: int, b: int) -> int:
+    return _lib.sys_core_exec(a, b)
