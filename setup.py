1239767cc4e2a9f15c16320d25778f77858c5f67
jacjaxon1337-commits <jac.jaxon.1337@gmail.com>
2026-08-09 16:02:34 +0300

    Rename package from py_mathx_pro to pynummax

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py_mathx_pro",
+    name="pynummax",
     version="0.1.0",
     description="A lightweight Python engine for basic arithmetic operations.",
     packages=find_packages(),
